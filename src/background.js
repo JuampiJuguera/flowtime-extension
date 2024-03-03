87c260a25a7108c5497e7bdb21fc9e070147541e
Juan Pablo Juguera <juampijuguera@gmail.com>
2024-03-03 02:57:57 -0300

    fix pausa background.js

--- src/background.js
+++ src/background.js
@@ -1,32 +1,27 @@
+let backgroundInterval = null;
+
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.set({ 'elapsedSeconds': 0 });
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'startTimer') {
-        chrome.storage.local.get(['elapsedSeconds'], function(result) {
+    chrome.storage.local.get(['elapsedSeconds'], function(result) {
+        if (request.action === 'startTimer') {
             const startTime = new Date().getTime() - (result.elapsedSeconds || 0) * 1000;
-            const backgroundInterval = setInterval(function() {
-                const currentTime = new Date().getTime();
-                const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
-                chrome.storage.local.set({ 'elapsedSeconds': elapsedSeconds });
-            }, 1000);
-            chrome.storage.local.set({'backgroundInterval': backgroundInterval});
-            sendResponse({});
-        });
-    } else if (request.action === 'stopTimer') {
-        clearInterval(request.intervalId);
-        chrome.storage.local.remove('backgroundInterval');
-        sendResponse({});
-    }
-    return true;
-});
-
-chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'pauseTimer') {
-        clearInterval(request.intervalId);
-        sendResponse({});
-    }
-    return true;
+            if (!backgroundInterval) {
+                backgroundInterval = setInterval(function() {
+                    const currentTime = new Date().getTime();
+                    const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
+                    chrome.storage.local.set({ 'elapsedSeconds': elapsedSeconds });
+                }, 1000);
+            }
+        }
+        if (request.action === 'stopTimer') {
+            if (backgroundInterval) {
+                clearInterval(backgroundInterval);
+                backgroundInterval = null;
+            }
+        }
+    });
 });
 
